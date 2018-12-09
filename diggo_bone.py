58bcf966bbd252c7e906df554ad00fe1e4864cac
jimmy <jimmy@pop-os.localdomain>
2018-12-09 20:11:23 -0300

    reading Diggo-dir

--- diggo_bone.py
+++ diggo_bone.py
@@ -4,16 +4,32 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import os
+from io import StringIO
+import json
+import list_files as lf
+
+diggo_dir = '../Diggo-files'
 
 list_of_files = [['Doggo.jpg', 2000],['potato.png',666]]
+json_string = lf.json_tree(diggo_dir)
+#io = StringIO(json_string)
+#files_dic = json.JSONDecoder()
+#objs = (files_dic.decode(json_string))
+objs = json.loads(json_string)
+
+for key, value in objs.iteritems():
+    print(str(key))
+    if 'size' in value:
+        print(value['size'])
 
 class MainWindow(Gtk.Window):
 
     def __init__(self):
         # Window Initialization
         Gtk.Window.__init__(self, title='Diggo Bone')
-        self.set_border_width(10)
         self.set_default_size(750, 300)
+        self.set_border_width(10)
+        
         # Components 
         nb = Gtk.Notebook()
         nb.set_tab_pos(Gtk.PositionType.TOP)
@@ -50,9 +66,16 @@ class MainWindow(Gtk.Window):
 
     def init_tree_view(self):
         ## Convert data to ListStore (lists that TreeView can display)
-        self.files_list_store = Gtk.TreeStore(str, int)
-        for item in list_of_files:
-            self.files_list_store.append(None, list(item))
+        self.files_list_store = Gtk.TreeStore(str, str)
+        
+        for key, value in objs.iteritems():
+            pair = [str(key), '']
+            if 'size' in value:
+               pair[1] = value['size']
+            self.files_list_store.append(None,pair)
+          
+        #for item in list_of_files:
+         #   self.files_list_store.append(None, list(item))
         ## TreeView is the item that is displayed
         self.files_tree_view = Gtk.TreeView(self.files_list_store)
         
--- list_files.py
+++ list_files.py
@@ -3,25 +3,21 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Return a list of folders and files given a path as argument.')
-parser.add_argument('path', metavar='P', type=str, nargs='+',
-                    help='a path to search for files')
-
-args = parser.parse_args()
-
 def rec_json_tree(path,tabs=1,sep='\t'):
     #tree = '{\n'
     tree =''
     children = os.listdir(path)
     for c in children:
         tree += (tabs*sep)+'"'+c+'":\n' # The name
+        timestamp = str(os.stat(path+'/'+c).st_mtime)
+        #timestamp = str(os.path.getmtime(path+'/'+c))
         if (os.path.isfile(path+'/'+c)):
             size = os.path.getsize(path+'/'+c)
-            timestamp = str(os.stat(path+'/'+c).st_mtime)
             tree+=tabs*sep+('{\n'
                 +(tabs+1)*sep+'"type": "file",\n'
                 +(tabs+1)*sep+'"status": "idle",\n'
-                +(tabs+1)*sep+'"size": "'+str(size)+' KB"\n'
+                +(tabs+1)*sep+'"size": "'+str(size)+' KB",\n'
+                +(tabs+1)*sep+'"timestamp": '+timestamp+'\n'
                 +tabs*sep+'}')
         elif not os.listdir(path+'/'+c):
             tree+=tabs*sep+('{\n'+
@@ -31,18 +27,25 @@ def rec_json_tree(path,tabs=1,sep='\t'):
             tree+=tabs*sep+('{\n'
                 +(tabs+1)*sep+'"type": "dir",\n'
                 +(tabs+1)*sep+'"status": "idle",\n'
+                +(tabs+1)*sep+'"timestamp": '+timestamp+',\n'
                 +rec_json_tree(path+'/'+c,tabs+1) # Files and subdirs
                 +tabs*sep+'}')
         if c is not children[-1]:
             tree+=','
         tree+='\n'
     return tree
-    #+(tabs-1)*sep+'close}'
 
 def json_tree(p):
     return '{\n'+rec_json_tree(p)+'}'
 
-for p in args.path:
-    print(json_tree(p))
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Return a list of folders and files given a path as argument.')
+    parser.add_argument('path', metavar='P', type=str, nargs='+',
+                        help='a path to search for files')
+    
+    args = parser.parse_args()
+
+    for p in args.path:
+        print(json_tree(p))
 
