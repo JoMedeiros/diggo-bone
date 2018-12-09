53d19029b07e0955b1d91352ed400dce2c2b6db9
Natalia <natalia_eletro@hotmail.com>
2018-12-09 17:28:31 -0300

    client.py modularizado

--- src/client/client.py
+++ src/client/client.py
@@ -2,55 +2,104 @@
 
 import socket
 import json
+#import calc_diff
 
-def ready_file(filename):
-	f = open(filename, 'r')
-	content = f.read()
+SERVER_IP = "127.0.0.1"
+PORT = 9999
 
-	# jsoning
-	payload = {"filename": filename, "content": content}
-	payload = json.dumps(payload)
-	buffsize = len(payload)
+def ready_payload(json_dict):
+	"""
+	dado dicionário, o transforma em numa string formatada
+	para .json e retorna tanto o tamanho essa string ("payload")
+	e o seu tamaho em uma string de bytes.
+	"""
+	payload = json.dumps(payload).encode()
+	payload_size = str(len(payload)).encode()
 
-	return payload, buffsize
+	return payload, payload_size
+
+
+# TODO : testar.
+def user_operations(comm):
+	"""
+	gera payload dado uma operação
+	"""
+	if comm == 'SYNC':
+		# gera dicionario de diferença
+		# retorna payload e payload_size
+		# envia payload
+		# espera payload de resposta com a tree sincronizada
+		# atualiza a tree atual e a ulta tree sincronizada
+		pass
+	#if comm == "RESET_SERVER":
+		# deleta tudo do servidor
+	pass
+
+
+def socket_talk(sock, payload, payload_size):
+	"""
+	realiza a comunicação cliente/servidor,
+	enviando primeiro o tamanho do payload principal
+	para preparar o servidor para recebêlo, e depois
+	o seu conteúdo.
+	"""
+
+	# send() buffsize
+	sock.send(payload_size)
+
+	# recv() server feedback
+	response = sock.recv(1024).decode()
+	if response != "True":
+		print("[ERROR!SERVER]: " + response)
+		return False
+
+	# send() payload
+	sock.send(payload)
+
+	# recv() server feedback
+	response = sock.recv(1024).decode()
+	if response != "True":
+		print("[ERROR!SERVER]: " + response)
+		return False
+
+	print("")
+	return True
+
 
 def socket_conn_client():
+	"""
+	cria e admnistar socket que se conectará ao servidor.
+	"""
+	
+	global SERVER_IP
+	global PORT
+
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect(("127.0.0.1", 9999))
+	sock.connect((SERVER_IP, PORT))
 
 	while True:
-		
-		# 1st ROUND
 
+		# get user operation
 		comm = input("> comm: ")
 		
+		# quit both on server and client
 		if comm == 'quit':
 			sock.send(comm.encode())
 			break
-		
-		buffcontent, buffsize = ready_file('test.txt')
-		
-		print("\tsending buffsize of:" + str(buffsize))
-		# send() buffsize
-		sock.send(str(buffsize).encode())
 
-		# recv() buffsize
-		print(sock.recv(1024).decode())
-
-
-		# 2nd ROUND
-
-		# send() content
-		print("\tsending content:" + buffcontent[10])
-		sock.send(buffcontent.encode())
-
-		# recv() message
-		print(sock.recv(1024).decode())
-
-		print("")
+		# decide user operation
+		payload, payload_size = user_operations(comm)
+		# speak to server
+		socket_talk(sock, payload, payload_size)
 
 	sock.close()
 
+
+
 if __name__ == "__main__":
+	
+	# global LAST_SYNC_TREE = json.loads(open("sync_tree.json", 'r').read())
+	# global CURENT_TREE = LAST_SYNC_TREE
+
 	socket_conn_client()
 	print('Exiting...')
