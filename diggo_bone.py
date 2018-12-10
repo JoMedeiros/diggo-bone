f060529b64a18093edc34d014e4b501aaf23bc14
jimmy <jimmy@pop-os.localdomain>
2018-12-10 01:09:49 -0300

    local reload button

--- diggo_bone.py
+++ diggo_bone.py
@@ -6,26 +6,23 @@ from gi.repository import Gtk
 import os
 from io import StringIO
 import json
+import shutil
+import time
 import list_files as lf
 
 diggo_dir = '../Diggo-files'
-
-list_of_files = [['Doggo.jpg', 2000],['potato.png',666]]
-json_string = lf.json_tree(diggo_dir)
-#io = StringIO(json_string)
-#files_dic = json.JSONDecoder()
-#objs = (files_dic.decode(json_string))
-objs = json.loads(json_string)
-
-for key, value in objs.iteritems():
-    print(str(key))
-    if 'size' in value:
-        print(value['size'])
-
+def sinc_dir():
+    while (True):
+        self.json_string = lf.json_tree(diggo_dir)
+        self.objs = json.loads(self.json_string)
+    
 class MainWindow(Gtk.Window):
 
     def __init__(self):
-        # Window Initialization
+        self.json_string = lf.json_tree(diggo_dir)
+        self.objs = json.loads(self.json_string)
+
+       # Window Initialization
         Gtk.Window.__init__(self, title='Diggo Bone')
         self.set_default_size(750, 300)
         self.set_border_width(10)
@@ -40,6 +37,7 @@ class MainWindow(Gtk.Window):
         loadfolderBtn = Gtk.Button(label='carregar diretorio')
         loadfileBtn = Gtk.Button(label='carregar arquivo')
         sincBtn = Gtk.Button(label='sincronizar com a BeagleBone')
+        localreloadBtn = Gtk.Button(label='atualizar alteracoes')
         ## Button Box
         btnBox = Gtk.HButtonBox()
         btnBox.set_border_width(10)
@@ -47,39 +45,37 @@ class MainWindow(Gtk.Window):
         loadfolderBtn.connect('clicked', self.on_folder_clicked)
         loadfileBtn.connect('clicked', self.on_file_load)
         sincBtn.connect('clicked', self.sincronizar)
+        localreloadBtn.connect('clicked', self.local_reload)
         
         self.init_tree_view()
         # Add TreeView to main layout
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.set_policy(
+        self.scrolled_window = Gtk.ScrolledWindow()
+        self.scrolled_window.set_policy(
             Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        scrolled_window.add(self.files_tree_view)
-        layout.pack_start(scrolled_window, True, True, 0)
+        self.scrolled_window.add(self.files_tree_view)
+        layout.pack_start(self.scrolled_window, True, True, 0)
 
-        btnBox.pack_start(loadfolderBtn, True, True, 0)
-        btnBox.pack_start(loadfileBtn, True, True, 0)
-        btnBox.pack_start(sincBtn, True, True, 0)
+        #btnBox.pack_start(loadfolderBtn, True, True, 0)
+        #btnBox.pack_start(loadfileBtn, True, True, 0)
+        #btnBox.pack_start(sincBtn, True, True, 0)
         
-        layout.pack_end(btnBox, True, True, 0)
+        grid = Gtk.Grid()
+        grid.attach(loadfolderBtn,0,0,1,1)
+        grid.attach(loadfileBtn,1,0,1,1)
+        grid.attach(sincBtn, 0,1,1,1)
+        grid.attach(localreloadBtn, 1,1,1,1)
+        layout.pack_end(grid, True, True, 0)
         nb.append_page(layout)
         self.add(nb)
 
     def init_tree_view(self):
         ## Convert data to ListStore (lists that TreeView can display)
-        self.files_list_store = Gtk.TreeStore(str, str)
+        self.files_list_store = Gtk.TreeStore(str, str, str)
         
-        for key, value in objs.iteritems():
-            pair = [str(key), '']
-            if 'size' in value:
-               pair[1] = value['size']
-            self.files_list_store.append(None,pair)
-          
-        #for item in list_of_files:
-         #   self.files_list_store.append(None, list(item))
-        ## TreeView is the item that is displayed
+        self.rec_add_node(None, self.objs)
         self.files_tree_view = Gtk.TreeView(self.files_list_store)
         
-        for i, col_title in enumerate(['Nome', 'Tamanho (em bytes)']):
+        for i, col_title in enumerate(['Nome', 'Tamanho (em bytes)', 'Alterado em']):
             # Render means how to draw the data
             renderer = Gtk.CellRendererText()
             # Create columns (title is column number)
@@ -87,8 +83,10 @@ class MainWindow(Gtk.Window):
             column.set_sort_column_id(i)
             # Add column to TreeView
             self.files_tree_view.append_column(column)
-    
+        print('tree view loaded') 
+
     def sincronizar(self, widget):
+        # This function must send JSON object to server
         print('Fazendo upload...')
 
     def on_file_load(self, widget):
@@ -99,9 +97,13 @@ class MainWindow(Gtk.Window):
         response = dialog.run()
 
         if response == Gtk.ResponseType.OK:
+            # @TODO : Add file in the JSON object
             path = dialog.get_filename()
             size = os.path.getsize(path)
-            self.files_list_store.append('./',(path.split('/')[-1], size))
+            name = path.split('/')[-1]
+            shutil.copy(path,diggo_dir)
+            timestamp = time.time()
+            self.files_list_store.append(None,[name, str(size)+' bytes', str(timestamp)])
         elif response == Gtk. ResponseType.CANCEL:
             dialog.destroy()
         dialog.destroy()
@@ -123,6 +125,7 @@ class MainWindow(Gtk.Window):
         dialog.add_filter(filter_any)
 
     def on_folder_clicked(self, widget):
+        # @TODO : Add files in the JSON object
         dialog = Gtk.FileChooserDialog("Escolha uma pasta para carregar os arquivos", 
                 self, Gtk.FileChooserAction.SELECT_FOLDER,
             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
@@ -143,7 +146,35 @@ class MainWindow(Gtk.Window):
             dialog.destroy()
 
         dialog.destroy()
+    
+    def rec_add_node(self, root, jsobj):
+        for key, value in jsobj.iteritems():
+            if type(value) in [str,int,float]:
+                return
+            pair = [str(key), '', value['date']]
 
+            if 'size' in value: # It's a file
+               pair[1] = value['size']
+               self.files_list_store.append(root,pair)
+            elif 'children' in value: # It's a dir
+                nr = self.files_list_store.append(root,pair)
+                self.rec_add_node(nr, value['children'])
+        
+    def local_reload(self, widget):
+        self.json_string = lf.json_tree(diggo_dir)
+        self.objs = json.loads(self.json_string)
+        self.files_list_store.clear()
+        self.rec_add_node(None, self.objs)
+        self.files_tree_view = Gtk.TreeView(self.files_list_store)
+        for i, col_title in enumerate(['Nome', 'Tamanho (em bytes)', 'Alterado em']):
+            # Render means how to draw the data
+            renderer = Gtk.CellRendererText()
+            # Create columns (title is column number)
+            column = Gtk.TreeViewColumn(col_title, renderer, text=i)
+            column.set_sort_column_id(i)
+            # Add column to TreeView
+            self.files_tree_view.append_column(column)
+        print('reload')
 
 window = MainWindow()
 window.connect('delete-event', Gtk.main_quit)
--- list_files.py
+++ list_files.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import datetime
 
 def rec_json_tree(path,tabs=1,sep=' '):
     #tree = '{\n'
@@ -10,14 +11,16 @@ def rec_json_tree(path,tabs=1,sep=' '):
     for c in children:
         tree += (tabs*sep)+'"'+c+'":\n' # The name
         timestamp = str(os.stat(path+'/'+c).st_mtime)
+        date = str(datetime.datetime.fromtimestamp(float(timestamp)))
         #timestamp = str(os.path.getmtime(path+'/'+c))
         if (os.path.isfile(path+'/'+c)):
             size = os.path.getsize(path+'/'+c)
             tree+=tabs*sep+('{\n'
                 +(tabs+1)*sep+'"type": "file",\n'
                 +(tabs+1)*sep+'"status": "idle",\n'
-                +(tabs+1)*sep+'"size": "'+str(size)+' KB",\n'
-                +(tabs+1)*sep+'"timestamp": '+timestamp+'\n'
+                +(tabs+1)*sep+'"size": "'+str(size)+' bytes",\n'
+                +(tabs+1)*sep+'"timestamp": '+timestamp+',\n'
+                +(tabs+1)*sep+'"date": "'+date+'"\n'
                 +tabs*sep+'}')
         elif not os.listdir(path+'/'+c):
             tree+=tabs*sep+('{\n'+
@@ -28,6 +31,7 @@ def rec_json_tree(path,tabs=1,sep=' '):
                 +(tabs+1)*sep+'"type": "dir",\n'
                 +(tabs+1)*sep+'"status": "idle",\n'
                 +(tabs+1)*sep+'"timestamp": '+timestamp+',\n'
+                +(tabs+1)*sep+'"date": "'+date+'",\n'
                 +(tabs+1)*sep+('"children":\n'
                 +(tabs+1)*sep+'{\n'
                 +rec_json_tree(path+'/'+c,tabs+2)) # Files and subdirs
